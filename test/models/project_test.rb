0e35c5b07cb999e7d32abb20ec772f87f361ad35
Jônatas Paganini <jonatasdp@gmail.com>
2013-10-18 23:00:21 -0300

    first validation on project model

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  ONE_PROJECT = -> { Project.create name: "My first project", url:  'my_project' }
+  test "should have name and url" do
+    assert_no_difference 'Project.count' do
+      Project.create
+    end
+    assert_difference 'Project.count', 1, &ONE_PROJECT
+  end
+
+  test "should have unique url" do
+    assert_difference 'Project.count', 1, &ONE_PROJECT
+    assert_no_difference 'Project.count', &ONE_PROJECT
+  end
 end
