c560f0dfdc312c18486eae972b4716848b28b289
Jônatas Paganini <jonatasdp@gmail.com>
2013-10-19 01:35:02 -0300

    pair with @jltafarel. decode base64 image on save image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,22 @@
 class Image < ActiveRecord::Base
   belongs_to :project
   has_attached_file :path, styles: {thumbnail: "64x64#", medium: '256x256#', large: '500x500#'}
+  before_save :decode_base64_image
+  attr_accessor :content_type, :original_filename, :image_data
+
+  protected
+  def decode_base64_image
+    if image_data && content_type && original_filename
+      decoded_data = Base64.decode64(image_data)
+      data = StringIO.new(decoded_data)
+      data.class_eval do
+        attr_accessor :content_type, :original_filename
+      end
+
+      data.content_type = content_type
+      data.original_filename = File.basename(original_filename)
+
+      self.path = data
+    end
+  end
 end
