1f131d757d4672a23369d660a2722a5a07b9121d
Jônatas Paganini <jonatasdp@gmail.com>
2013-10-18 21:06:33 -0300

    #railsrumble here we go :+1:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reinventto::Application.config.secret_key_base = '343d2edc4d41d3ccd0ba03b3ed8d5795acf8ebf08aad4948d1baa2d7e963c24bd03ca08e289ddbe4b6b1e56c3306d88472023c119fba566528c40a856fe412ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reinventto::Application.config.session_store :cookie_store, key: '_reinventto_session'
