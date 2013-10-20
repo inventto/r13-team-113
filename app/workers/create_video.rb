63e37faf852015be5550e921f9fadae8ac8734d1
Jônatas Paganini <jonatasdp@gmail.com>
2013-10-20 19:20:24 -0200

    using logger

--- app/workers/create_video.rb
+++ app/workers/create_video.rb
@@ -1,14 +1,19 @@
+Resque.logger = Logger.new("resque_log_file.log")
 class CreateVideo
-  Resque.logger = Logger.new("resque_log_file.log")
   @queue = :create_video
   def self.perform(project_id)
+    Resque.logger.info "Generating video for project id: #{project_id}" 
     project = Project.find(project_id)
     video_output_path = project.video_output_path
+    Resque.logger.info "video output: #{video_output_path}" 
     File.delete project.video_output_path if File.exists? video_output_path
     Dir.foreach(Dir.pwd) do |img|
       `convert -antialias -delay 25 '#{img}' #{img.split(/\.png$/).first}.mpeg`
     end
     `cat *.mpeg | ffmpeg -y -i - #{video_output_path}`
     `rm -f *.mpeg`
+  rescue
+    Resque.logger.info "Error: #{$!}"
+    Resque.logger.info "StackTRACE: #{$@.join("\n")}"
   end
 end
