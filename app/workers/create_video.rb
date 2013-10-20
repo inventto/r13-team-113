e515cd6ae312d50ae536e031f7778ce899383e67
Jônatas Paganini <jonatasdp@gmail.com>
2013-10-20 19:54:11 -0200

    create video worker

--- app/workers/create_video.rb
+++ app/workers/create_video.rb
@@ -6,14 +6,16 @@ class CreateVideo
     project = Project.find(project_id)
     video_output_path = project.video_output_path
     Resque.logger.info "video output: #{video_output_path}" 
-    File.delete project.video_output_path if File.exists? video_output_path
-    Dir.foreach(project.dir) do |img|
-      `convert -antialias -delay 25 '#{img}' #{img.split(/\.png$/).first}.mpeg`
+    begin
+      File.delete project.video_output_path if File.exists? video_output_path
+      Dir.foreach(project.dir) do |img|
+        `convert -antialias -delay 25 '#{img}' #{img.split(/\.png$/).first}.mpeg`
+      end
+      `cat #{project.dir}/*.mpeg | ffmpeg -y -i - #{video_output_path}`
+      `rm -f #{project.dir}/*.mpeg`
+    rescue
+      Resque.logger.info "Error: #{$!}"
+      Resque.logger.info "StackTRACE: #{$@.join("\n")}"
     end
-    `cat *.mpeg | ffmpeg -y -i - #{video_output_path}`
-    `rm -f *.mpeg`
-  rescue
-    Resque.logger.info "Error: #{$!}"
-    Resque.logger.info "StackTRACE: #{$@.join("\n")}"
   end
 end
