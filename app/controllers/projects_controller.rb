4e271391ef6c315da84f28f7ff88e81468cca5d2
Jônatas Paganini <jonatasdp@gmail.com>
2014-02-03 12:11:30 -0200

    filtering by projects with images. show images count too

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.all.with_images
   end
   def decide
     if cookies[:unique_url]
